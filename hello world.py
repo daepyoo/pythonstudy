5d234b9827eff1e35c92383257bcde62fc66c3b3
daepyoo <hsyt3337@hotmail.com>
2023-01-19 20:09:41 +0900

    Update hello world.py
    230119

--- hello world.py
+++ hello world.py
@@ -1,1 +1,42 @@
-print("hello world")
+##word=10
+##word2="s10.2"
+##print(id(word))
+##print(id(word2))
+## 10 int
+## 10.34 float
+## "yeah" str
+##print(type(word2))
+##test="hello world"
+##print(test[0:])
+##print(test[:-1])
+##[이상:미만] [0 이상: 5 미만]
+##greeting="hello minsu"
+##print(greeting[:-1])
+##greeting2=greeting + " 안녕 민수야"
+##print(greeting2)
+##len(greeting)
+bitcoin_cur=9744000
+ripple_cur=711
+bitcoin=10
+ripple=430
+total=bitcoin_cur*bitcoin+ripple_cur*ripple
+print(total)
+currency3="btc_krw"
+print(currency3[0:3])
+hold=["bit", "rip", "eth"]
+print(hold[0][1])
+hold[0]="doge"
+print(hold)
+hold.append("1")
+print(hold)
+hold.insert(1,"2")
+print(hold)
+hold.pop(0)
+print(hold)
+
+##[리스트], {딕셔너리, 인덱스 없음}, (튜플)
+
+tutu = (1,2,3)
+print(tutu)
+
+
